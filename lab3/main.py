32ce7e9c52b3b0051a1271ed37fb3aa571b872d2
Alexandre425 <alexandre.d.rodrigues@tecnico.ulisboa.pt>
2020-11-10 12:06:54 +0000

    dataset added

--- lab3/main.py
+++ lab3/main.py
@@ -1,8 +1,15 @@
 from tensorflow.keras.datasets import fashion_mnist as fmds
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
     dataset = fmds.load_data()
-    print(dataset)
-    print()
+    img = dataset[0][0][1]
+
+    new_img = [[c / 255 for c in l] for l in img]
+
+    plt.imshow(img)
+    plt.show()
+
+    print(new_img)
+    
