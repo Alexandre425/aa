fd9e92591d35e8516a38570964c3e24370454ac3
Alexandre425 <alexandre.d.rodrigues@tecnico.ulisboa.pt>
2020-11-10 17:29:32 +0000

    added extra axis to input data, loaded layer library

--- lab3/main.py
+++ lab3/main.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.datasets import fashion_mnist as fmds
 from tensorflow import keras as keras
+from tensorflow.keras import layers as ly
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -14,6 +15,7 @@ if __name__ == "__main__":
     data_test = dataset[1]
 
     try:
+        # Load the files if they exist
         train_x = np.load(FILE_TRAIN_X)
         train_y = np.load(FILE_TRAIN_Y)
         test_x = np.load(FILE_TEST_X)
@@ -21,15 +23,18 @@ if __name__ == "__main__":
     except:
         print("Files don't exist, generating...")
         # Normalize the input data
-        train_x = np.array([[[c / 255 for c in l] for l in img] for img in data_train[0]])
-        test_x  = np.array([[[c / 255 for c in l] for l in img] for img in data_test[0]])
+        train_x = [np.expand_dims([[c / 255 for c in l] for l in img], 2) for img in data_train[0]]
+        test_x  = [np.expand_dims([[c / 255 for c in l] for l in img], 2) for img in data_test[0]]
         # Convert the output data to one-hot encoding
         train_y = np.array([keras.utils.to_categorical(y, num_classes=10) for y in data_train[1]])
         test_y  = np.array([keras.utils.to_categorical(y, num_classes=10) for y in data_test[1]])
+        # Save the data to files
         np.save(FILE_TRAIN_X, train_x)
         np.save(FILE_TRAIN_Y, train_y)
         np.save(FILE_TEST_X, test_x)
         np.save(FILE_TEST_Y, test_y)
+
+    print("Data loaded")
 
     #######
     # MLP #
@@ -39,5 +44,8 @@ if __name__ == "__main__":
     # CNN #
     #######
 
-    
-    
+    print(train_x.shape)
+
+    model = keras.models.Sequential([
+        ly.Conv2D(filters=16, kernel_size=3)
+    ])
